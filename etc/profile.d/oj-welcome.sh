523e4c6414e04728b5139974e56ce77d4fe8c0cc
Ehab El-Gedawy <ehabsas@gmail.com>
2014-02-18 08:45:58 +0200

    new bash welcome message

--- etc/profile.d/oj-welcome.sh
+++ etc/profile.d/oj-welcome.sh
@@ -1,1 +1,19 @@
-printf  "welcome $(cat /etc/issue | head -n1)\n"
+
+#http://ubuntuforums.org/showthread.php?t=2028113&p=12110679#post12110679
+echo_c()
+{
+  w=$(stty size | cut -d" " -f2)       # width of the terminal
+  l=${#1}                              # length of the string
+  printf "%"$((l+(w-l)/2))"s\n" "$1"   # print string padded to proper width (%Ws)
+}
+echo_c_colored()
+{
+  w=$(stty size | cut -d" " -f2)       # width of the terminal
+  l=${#1}                              # length of the string
+  printf "%"$(((l+6)+(w-l)/2))"s\n" "$1"   # print string padded to proper width (%Ws)
+}
+echo
+echo_c  "********************************************"
+echo_c_colored  "** $(tput setaf 2)wellcome $(cat /etc/issue | head -n1)$(tput sgr0) **"
+echo_c  "********************************************"
+echo
