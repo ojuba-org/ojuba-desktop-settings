01e1a344e45e0e6ce0d70d71f57e12b2339696df
Mosaab Alzoubi <moceap@hotmail.com>
2014-02-22 20:14:04 +0300

    Update oj-welcome.sh

--- etc/profile.d/oj-welcome.sh
+++ etc/profile.d/oj-welcome.sh
@@ -12,9 +12,9 @@ echo_c_colored()
   printf "%"$(((l+6)+(w-l)/2))"s\n" "$1"   # print string padded to proper width (%Ws)
 }
 echo
-echo_c  "********************************************"
+echo_c  "*********************************************"
 echo_c_colored  "** $(tput setaf 2)Welcome To Ojuba 35 (Aljazair) Terminal$(tput sgr0) **"
 # FIXME: To use /etc/issue after Arabic support in terminal.
 #echo_c_colored  "** $(tput setaf 2)wellcome $(cat /etc/issue | head -n1)$(tput sgr0) **"
-echo_c  "********************************************"
+echo_c  "*********************************************"
 echo
